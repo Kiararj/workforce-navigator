25baa39159bb51048dad394cce8fe79ae8060cf2
Kiara Johnson <kiarajohnson91@gmail.com>
2023-09-21 14:35:07 -0500

    added if else statements and began functions

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const db = require('./config/connection');
 const prompts = [
     { type: "list",
       message: "What would you like to do?",
-      choices: ["View All Departments", "View All Roles", "View All Employees", "Add a Department", "Add a Role", "Add an Employee", "Update an Employee Role"]
+      choices: ["View All Departments", 
+      "View All Roles", 
+      "View All Employees", 
+      "Add a Department", 
+      "Add a Role", 
+      "Add an Employee", 
+      "Update an Employee Role"],
+      name: "answer",
     },
 ];
 
@@ -15,8 +22,55 @@ function init() {
     .prompt(prompts)
     .then((response) => {
         console.log(response)
-})
+});
+// For loop to determine which SQL function to run
+if (response.answer === "View All Departments"){
+  viewAllDepartments();
+} else if (response.answer === "View All Roles"){
+  viewAllRoles();
+} else if (response.answer === "View All Employees"){
+  viewAllEmployees();
+} else if (response.answer === "Add a Department"){
+  addDepartment();
+} else if (response.answer === "Add a Role"){
+  addRole();
+} else if (response.answer === "Add an Employee"){
+  addEmployee();
+} else if (response.answer === "Update an Employee Role"){
+  updateEmployeeRole();
 };
+};
+
+const viewAllDepartments = () => {
+  db.query('SELECT * FROM department;', );
+};
+
+const viewAllRoles = () => {
+  db.query('SELECT * FROM roles;', );
+};
+
+const viewAllEmployees = () => {
+  db.query('SELECT * FROM employees;', );
+};
+
+// const addDepartment = () => {
+//    db.query('INSERT INTO department (id, name) VALUES ('', '');', );
+// };
+
+
+// -- Add a department --
+// INERT INTO department (id, name)
+// VALUES ('', '');
+
+// -- Add an employee --
+// INSERT INTO employee (title, salary, department_id)
+// VALUES ('', '', '');
+
+// -- Update an employee --
+// UPDATE employee
+// SET title = '', salary = '', department_id = ''
+// WHERE role_id = ''; 
+
 
 // Function call to initialize app
 init();
