c9d54dc3fd43af5a6a4bab6d21ea211cec76ff33
Kiara Johnson <kiarajohnson91@gmail.com>
2023-10-12 14:42:12 -0500

    added adddepartment function, fixed others

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+// Imports required packages
 const inquirer = require("inquirer");
 const mysql = require('mysql2');
 const db = require('./config/connection');
-
+// Prompts for the user to choose an action 
 const prompts = [
-    { type: "list",
+      {type: "list",
       message: "What would you like to do?",
       choices: ["View All Departments", 
       "View All Roles", 
@@ -13,7 +14,7 @@ const prompts = [
       "Add an Employee", 
       "Update an Employee Role"],
       name: "answer",
-    },
+  },
 ];
 
 // Function to initialize the app
@@ -21,41 +22,80 @@ function init() {
     inquirer
     .prompt(prompts)
     .then((response) => {
-        console.log(response)
-});
-// For loop to determine which SQL function to run
-if (response.answer === "View All Departments"){
-  viewAllDepartments();
-} else if (response.answer === "View All Roles"){
-  viewAllRoles();
-} else if (response.answer === "View All Employees"){
-  viewAllEmployees();
-} else if (response.answer === "Add a Department"){
-  addDepartment();
-} else if (response.answer === "Add a Role"){
-  addRole();
-} else if (response.answer === "Add an Employee"){
-  addEmployee();
-} else if (response.answer === "Update an Employee Role"){
-  updateEmployeeRole();
-};
-};
+      if (response.answer === "View All Departments"){
+        viewAllDepartments();
+        console.log("View all departments");
+      } else if (response.answer === "View All Roles"){
+        viewAllRoles();
+      } else if (response.answer === "View All Employees"){
+        viewAllEmployees();
+      } else if (response.answer === "Add a Department"){
+        addDepartment();
+      } else if (response.answer === "Add a Role"){
+        addRole();
+      } else if (response.answer === "Add an Employee"){
+        addEmployee();
+      } else if (response.answer === "Update an Employee Role"){
+        updateEmployeeRole();
+      }
+    }
+)};
 
+
+// Functions that create tables, add department, add role, add employee, and update employee
 const viewAllDepartments = () => {
-  db.query('SELECT * FROM department;', );
+  db.query('SELECT * FROM department;', (err, results) => {
+    if(err){
+      throw err;
+    }
+    console.table(results);
+    init();
+  });
 };
 
 const viewAllRoles = () => {
-  db.query('SELECT * FROM roles;', );
+  db.query('SELECT * FROM role;', (err, results) => {
+    if (err){
+      throw err;
+    }
+    console.table(results);
+    init();
+  });
 };
 
 const viewAllEmployees = () => {
-  db.query('SELECT * FROM employees;', );
+  db.query('SELECT * FROM employee;', (err, results) => {
+    if (err){
+      throw err;
+    }
+    console.table(results);
+    init();
+  });
 };
 
-// const addDepartment = () => {
-//    db.query('INSERT INTO department (id, name) VALUES ('', '');', );
-// };
+const addDepartment = () => {
+  inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'departmentName',
+      message: 'What would you like to name the department?'
+    }
+  ])
+  .then(answer => {
+    const departmentName = answer.departmentName;
+    db.query('INSERT INTO department (name) VALUES (?)', [departmentName], (err, result) => {
+      if (err) {
+        throw err;
+      }
+      console.log(`Department added!`);
+      init();
+    });
+  })
+  .catch(error => {
+    console.error(error);
+  });
+ };
 
 
 // -- Add a department --
